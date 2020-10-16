45af168420c858875af17765957131028f0d74f3
TBS093A <zukkamil.44@gmail.com>
2020-10-16 12:14:48 +0200

    change packages list -> packages.sh

--- packages.sh
+++ packages.sh
@@ -3,7 +3,8 @@ pip install djangorestframework
 pip install channels_redis
 pip install drf_yasg
 pip install pyjwt
-pip install celery
+# pip install celery
+pip install gevent
 pip install redis
 pip install channels_redis
 pip install djangochannelsrestframework
