81f20f47f9a8d14e1e20da0606f4e346bd2a0284
TBS093A <zukkamil.44@gmail.com>
2020-10-19 16:03:14 +0200

    change namespaces (renderAsync) -> render app && add valid methods (GreenletTask)

--- work/render/renderAsync/GreenletTask.py
+++ work/render/renderAsync/GreenletTask.py
@@ -1,21 +1,29 @@
+from gevent import Greenlet
+import greenlet
+
 from gevent_tasks import Task
 import uuid
 
-
-class GeenletTask(Task):
+class GreenletTask(Task):
     """
     Own greenlet class for rendering in background
     """
 
     def __init__(self, func, **kwargs):
         self.taskKey = f'RenderAsync/{ uuid.uuid4() }'
-        self.task = Task(self.taskKey, func)
+        self.task = Task(self.taskKey, func, kwargs=kwargs)
 
     def getTaskId(self) -> str:
         return self.taskKey
 
-    def __getitem__(self) -> Task:
+    def getTask(self) -> Task:
         return self.task
+
+    def _start(self):
+        self.task.start()
+
+    def _stop(self):
+        self.task.stop()
 
     def __repr__(self) -> str:
         return str(self.task)
@@ -33,10 +41,10 @@ class GreenletTaskManager():
     def __getitem__(self, position):
         return self._taskList[position]
 
-    def append(self, item: RenderGeenlet):
+    def append(self, item: GreenletTask):
         self._taskList.append(item)
 
-    def remove(self, item: RenderGeenlet):
+    def remove(self, item: GreenletTask):
         self._taskList.remove(item)
 
     def __repr__(self):
