db4d37ccbad2402bc4917f15dd77990c3988b92a
TBS093A <zukkamil.44@gmail.com>
2020-10-19 15:28:39 +0200

    upgrade gevent stuff -> add greenlet background task functionality

--- work/render/threading/renderGreenlet.py
+++ work/render/threading/renderGreenlet.py
@@ -1,40 +1,46 @@
-from gevent import Greenlet
-import gevent
+from gevent_tasks import Task
+import uuid
 
 
-class RenderGeenlet(Greenlet):
+class GeenletTask(Task):
     """
     Own greenlet class for rendering in background
     """
 
     def __init__(self, func, **kwargs):
-        Greenlet.__init__(self)
-        self.thread = Greenlet.spawn(func, **kwargs)
+        self.taskKey = f'RenderAsync/{ uuid.uuid4() }'
+        self.task = Task(self.taskKey, func)
+
+    def getTaskId(self) -> str:
+        return self.taskKey
+
+    def __getitem__(self) -> Task:
+        return self.task
+
+    def __repr__(self) -> str:
+        return str(self.task)
 
 
-class GreenletSingleton():
+# Singleton design pattern
+
+class GreenletTaskManager():
     """
     Global class for storage all running threads / greenlets
     """
 
-    _greenletList = []
+    _taskList = []
 
     def __getitem__(self, position):
-        return self._greenletList[position]
+        return self._taskList[position]
 
     def append(self, item: RenderGeenlet):
-        self._greenletList.append(item)
+        self._taskList.append(item)
 
     def remove(self, item: RenderGeenlet):
-        self._greenletList.remove(item)
+        self._taskList.remove(item)
 
     def __repr__(self):
         info = ''
-        for greenlet in self._greenletList:
-            info += '{:>15} | {:>15} | {:>15} | {:>15}'.format(
-                greenlet,
-                str(greenlet.started()), 
-                str(greenlet.ready()),
-                str(greenlet.successful())    
-            )
+        for greenlet in self._taskList:
+            info += str(greenlet) + '\n'
         return info
