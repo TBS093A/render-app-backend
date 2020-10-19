ec752407535ce0bb47c78af948ad85af44f4eba3
TBS093A <zukkamil.44@gmail.com>
2020-10-19 15:13:17 +0200

    install gevent-tasks

--- packages.sh
+++ packages.sh
@@ -4,6 +4,7 @@ pip install channels_redis
 pip install drf_yasg
 pip install pyjwt
 pip install gevent
+pip install gevent-tasks
 pip install redis
 pip install channels_redis
 pip install djangochannelsrestframework
