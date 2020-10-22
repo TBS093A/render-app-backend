b7076db876a1d8b52471ed1273de2886056762f4
TBS093A <zukkamil.44@gmail.com>
2020-10-22 12:44:35 +0200

    create render methods in consumer strategy design pattern -> render app

--- work/render/consumers.py
+++ work/render/consumers.py
@@ -42,16 +42,60 @@ class AbstractConsumer(AsyncWebsocketConsumer, ABC):
 class RenderSingleImageConsumer(AbstractConsumer):
 
     async def render(self):
-        pass
+        renderSingleImage = RenderSingleImage(
+            self.params['fileName'] + '.blend'
+        )
+        await renderSingleImage.render(
+            int(self.params['setID']),
+            float(self.params['rotate']),
+            int(self.params['nameSeries']),
+            int(self.params['cameraID']),
+            resolution=(
+                int(self.params['resolutionX']), 
+                int(self.params['resolutionY'])
+            )
+        )
+        await self.send({
+            'info': 'render success',
+            'details': self.params
+        })
 
 
 class RenderSingleSetConsumer(AbstractConsumer):
 
     async def render(self):
-        pass
+        renderSingleSet = RenderSingleSet(
+            self.params['fileName'] + '.blend'
+        )
+        for renderImage in renderSingleSet(
+            int(self.params['setID']),
+            int(self.params['cameraID']),
+            resolution=(
+                int(self.params['resolutionX']), 
+                int(self.params['resolutionY'])
+            ),
+            angle=float(self.params['angle'])
+        ):
+            await self.send({
+                'info': renderImage,
+                'details': self.params
+            })
 
 
 class RenderAllConsumer(AbstractConsumer):
 
     async def render(self):
-        pass
+        renderAllSets = RenderAllSets(
+            self.params['fileName'] + '.blend'
+        )
+        for renderSet in renderAllSets(
+            resolution=(
+                int(self.params['resolutionX']), 
+                int(self.params['resolutionY'])
+            ),
+            angle=float(self.params['angle'])
+        ):
+            await self.send({
+                'info': renderSet,
+                'details': self.params
+            })
