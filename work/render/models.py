acd93d28cb990237a12baeede4060cef47d77d1d
TBS093A <zukkamil.44@gmail.com>
2021-02-16 11:01:25 +0100

    /render -> upgrade models

--- work/render/models.py
+++ work/render/models.py
@@ -17,7 +17,7 @@ class Model(models.Model):
 
 class RenderAbstract(models.Model):
     name = models.CharField(max_length=30)
-    model = models.ForeignKey(Model, on_delete=models.SET_NULL)
+    model = models.ForeignKey(Model, on_delete=models.CASCADE)
 
     def share_archive_of_renders(self):
         file = open(f'renders/{self.name}.7z', 'r')
@@ -42,48 +42,46 @@ class RenderSet(RenderImage):
     angle = models.FloatField()
 
 
-class Vectors(models.Model):
-    scale   = models.FloatField()
-    y       = models.FloatField()
-    x       = models.FloatField()
-    z       = models.FloatField()
+class RenderImageByVector(RenderImage):
+    pass
 
+
+class RenderSetByVector(RenderSet):
+    pass
+
+
+default_vector = '{"scale": 0.0, "y": 0.0, "x": 0.0, "z": 0.0}'
 
 class FingerVectorsDictAbstract(models.Model):
-    IK_nadgarstek_R     = models.ForeignKey(Vectors, on_delete=models.CASCADE, null=True)
-    
-    IK_joint3_R         = models.ForeignKey(Vectors, on_delete=models.CASCADE, null=True)
-    IK_joint4_R         = models.ForeignKey(Vectors, on_delete=models.CASCADE, null=True)
-    IK_joint5_R         = models.ForeignKey(Vectors, on_delete=models.CASCADE, null=True)
-    IK_joint6_R         = models.ForeignKey(Vectors, on_delete=models.CASCADE, null=True)
-    
-    IK_maly_1_R         = models.ForeignKey(Vectors, on_delete=models.CASCADE)
-    IK_maly_2_R         = models.ForeignKey(Vectors, on_delete=models.CASCADE)
-    IK_maly_3_R         = models.ForeignKey(Vectors, on_delete=models.CASCADE)
-    
-    IK_serdeczny_1_R    = models.ForeignKey(Vectors, on_delete=models.CASCADE)
-    IK_serdeczny_2_R    = models.ForeignKey(Vectors, on_delete=models.CASCADE)
-    IK_serdeczny_3_R    = models.ForeignKey(Vectors, on_delete=models.CASCADE)
-    
-    IK_srodkowy_1_R     = models.ForeignKey(Vectors, on_delete=models.CASCADE)
-    IK_srodkowy_2_R     = models.ForeignKey(Vectors, on_delete=models.CASCADE)
-    IK_srodkowy_3_R     = models.ForeignKey(Vectors, on_delete=models.CASCADE)
-    
-    IK_wskazujacy_1_R   = models.ForeignKey(Vectors, on_delete=models.CASCADE)
-    IK_wskazujacy_2_R   = models.ForeignKey(Vectors, on_delete=models.CASCADE)
-    IK_wskazujacy_3_R   = models.ForeignKey(Vectors, on_delete=models.CASCADE)
-    
-    IK_kciuk_0_R        = models.ForeignKey(Vectors, on_delete=models.CASCADE)         
-    IK_kciuk_1_R        = models.ForeignKey(Vectors, on_delete=models.CASCADE)
-    IK_kciuk_2_R        = models.ForeignKey(Vectors, on_delete=models.CASCADE)
+    IK_nadgarstek_R     = models.CharField(max_length=100, verbose_name=default_vector)
+    IK_joint3_R         = models.CharField(max_length=100, verbose_name=default_vector)
+    IK_joint4_R         = models.CharField(max_length=100, verbose_name=default_vector)
+    IK_joint5_R         = models.CharField(max_length=100, verbose_name=default_vector)
+    IK_joint6_R         = models.CharField(max_length=100, verbose_name=default_vector)
+    IK_maly_1_R         = models.CharField(max_length=100, verbose_name=default_vector)
+    IK_maly_2_R         = models.CharField(max_length=100, verbose_name=default_vector)
+    IK_maly_3_R         = models.CharField(max_length=100, verbose_name=default_vector)
+    IK_serdeczny_1_R    = models.CharField(max_length=100, verbose_name=default_vector)
+    IK_serdeczny_2_R    = models.CharField(max_length=100, verbose_name=default_vector)
+    IK_serdeczny_3_R    = models.CharField(max_length=100, verbose_name=default_vector)
+    IK_srodkowy_1_R     = models.CharField(max_length=100, verbose_name=default_vector)
+    IK_srodkowy_2_R     = models.CharField(max_length=100, verbose_name=default_vector)
+    IK_srodkowy_3_R     = models.CharField(max_length=100, verbose_name=default_vector)
+    IK_wskazujacy_1_R   = models.CharField(max_length=100, verbose_name=default_vector)
+    IK_wskazujacy_2_R   = models.CharField(max_length=100, verbose_name=default_vector)
+    IK_wskazujacy_3_R   = models.CharField(max_length=100, verbose_name=default_vector)
+    IK_kciuk_0_R        = models.CharField(max_length=100, verbose_name=default_vector)         
+    IK_kciuk_1_R        = models.CharField(max_length=100, verbose_name=default_vector)
+    IK_kciuk_2_R        = models.CharField(max_length=100, verbose_name=default_vector)
+
+    class Meta:
+        abstract = True
 
 
-class FingerVectorsDict(models.Model):
+class FingerVectorsDictImage(FingerVectorsDictAbstract):
     render = models.ForeignKey(RenderImageByVector, on_delete=models.CASCADE)
 
 
-class RenderImageByVector(RenderImage, FingerVectorsDict):
-    pass
+class FingerVectorsDictSet(FingerVectorsDictAbstract):
+    render = models.ForeignKey(RenderSetByVector, on_delete=models.CASCADE)
 
-class RenderSetByVector(RenderSet, FingerVectorsDict):
-    pass
