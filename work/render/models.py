4f237c20eae6dcd1952e92cbe0da048dbb1d5d8b
TBS093A <zukkamil.44@gmail.com>
2021-02-16 10:24:10 +0100

    Render -> /render -> refactor models

--- work/render/models.py
+++ work/render/models.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from work.account.models import Account
 
 class Model(models.Model):
+    
     name = models.CharField(max_length=255)
     sets = models.IntegerField()
     cameras = models.IntegerField()
@@ -13,17 +14,40 @@ class Model(models.Model):
 
     user = models.ForeignKey(Account, on_delete=models.CASCADE)
 
-class RenderSet(models.Model):
+
+class RenderAbstract(models.Model):
     name = models.CharField(max_length=30)
-    setID = models.IntegerField()
-    cameraID = models.IntegerField()
-    angle = models.FloatField()
-    images_width = models.IntegerField()
-    images_height = models.IntegerField()
-    
-    model = models.ForeignKey(Model, on_delete=models.SET_NULL, null=True)
+    model = models.ForeignKey(Model, on_delete=models.SET_NULL)
 
     def share_archive_of_renders(self):
         file = open(f'renders/{self.name}.7z', 'r')
         file_download = FileResponse(file, content_type='archive/7zip')
         file_download['Content-Lenght']
+
+    class Meta:
+        abstract = True
+
+
+class RenderAll(RenderAbstract):
+    images_width = models.IntegerField()
+    images_height = models.IntegerField()
+
+
+class RenderImage(RenderAll):
+    setID = models.IntegerField()
+    cameraID = models.IntegerField()
+    
+
+class RenderSet(RenderImage):
+    angle = models.FloatField()
+
+
+class FingerVectorsDictAbstract(models.Model):
+    pass
+
+class FingerVectorsDict(FingerVectorsDict):
+    render = models.ForeignKey(RenderImageByVector, on_delete=models.CASCADE)
+
+
+class RenderImageByVector(RenderAbstract):
+    pass
